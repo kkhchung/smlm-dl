55f2a2957e0e3876fde044176db81ff14ce17b38
kkhchung <39562183+kkhchung@users.noreply.github.com>
2021-11-12 22:04:42 -0500

    Added 3D PSF dataset.

--- smlm_dl/dataset.py
+++ smlm_dl/dataset.py
@@ -7,12 +7,17 @@ class SimulatedPSFDataset(Dataset):
     """
     Base class.
     """
-    def __init__(self, size=(32, 32), length=512, psf_params={'A':[500,2000], 'bg':[0,100]}, noise_params={'poisson':True, 'gaussian':100}, normalize=True, *args, **kwargs):
+    def __init__(self, size=(32, 32), length=512, random_z=False, psf_params={'A':[500,2000], 'bg':[0,100]},
+                 noise_params={'poisson':True, 'gaussian':10}, normalize=True, *args, **kwargs):
         Dataset.__init__(self)
         
-        self.shifts = np.stack([np.random.uniform(-size[0]/3, size[0]/3, length),
-                                np.random.uniform(-size[1]/3, size[1]/3, length),
-                                ], axis=-1)
+        self.random_z = random_z
+        self.shifts = list([np.random.uniform(-size[0]/3, size[0]/3, length),
+                            np.random.uniform(-size[1]/3, size[1]/3, length),                                
+                            ])
+        if self.random_z:
+            self.shifts.append(np.random.uniform(-2*np.pi, 2*np.pi, length))
+        self.shifts = np.stack(self.shifts, axis=-1)
         
         psfs = self.generate_psfs(size, length, self.shifts, psf_params, *args, **kwargs) #assumes normalized from 0 to 1
         
@@ -63,7 +68,7 @@ class SimulatedPSFDataset(Dataset):
 class Gaussian2DPSFDataset(SimulatedPSFDataset):
     def __init__(self, size=(32, 32), length=512, psf_params={'A':[500,2000], 'bg':[0,100], 'sig_x':[5,5], 'sig_y':[5,5]},
                  noise_params={'poisson':True, 'gaussian':100}, *args, **kwargs):
-        SimulatedPSFDataset.__init__(self, size, length, psf_params, noise_params, *args, **kwargs)
+        SimulatedPSFDataset.__init__(self, size, length, False, psf_params, noise_params, *args, **kwargs)
         
     def generate_psfs(self, size, length, shifts, psf_params, *args, **kwargs):
         xs = np.arange(0, size[0]) - 0.5*(size[0]-1)
@@ -77,18 +82,18 @@ class Gaussian2DPSFDataset(SimulatedPSFDataset):
         ret /= ret.max(axis=(1,2), keepdims=True)
         return ret
     
-
-class FourierOptics2DPSFDataset(SimulatedPSFDataset):
-    def __init__(self, size=(32, 32), length=512, psf_params={'A':[500,2000], 'bg':[0,100], 'sig_x':[2.5,2.5], 'sig_y':[2.5,2.5]},
+    
+class FourierOpticsPSFDataset(SimulatedPSFDataset):
+    def __init__(self, size=(32, 32), length=512, random_z=False, psf_params={'A':[500,2000], 'bg':[0,100], 'sig_x':[2.5,2.5], 'sig_y':[2.5,2.5]},
                  psf_zerns={},
                  noise_params={'poisson':True, 'gaussian':100},
                 *args, **kwargs):
         
-        kx = np.fft.fftfreq(4*size[0])
-        ky = np.fft.fftfreq(4*size[1])
-        self.kx, self.ky = np.meshgrid(kx, ky, indexing='ij')
+        kx = np.fft.fftshift(np.fft.fftfreq(4*size[0]))
+        ky = np.fft.fftshift(np.fft.fftfreq(4*size[1]))
+        self.KX, self.KY = np.meshgrid(kx, ky, indexing='ij')
         
-        SimulatedPSFDataset.__init__(self, size, length, psf_params, noise_params, psf_zerns=psf_zerns, *args, **kwargs)
+        SimulatedPSFDataset.__init__(self, size, length, random_z, psf_params, noise_params, psf_zerns=psf_zerns, *args, **kwargs)
         
         
     def generate_psfs(self, size, length, shifts, psf_params, *args, **kwargs):
@@ -99,15 +104,22 @@ class FourierOptics2DPSFDataset(SimulatedPSFDataset):
         pupil_mag = np.sqrt(1-np.minimum(R, 1)**2)
         # pupil_phase = np.zeros(pupil_mag.shape)
         pupil_phase = self.calculate_pupil_phase(R*(R<=1), np.arctan2(US, VS), kwargs.pop("psf_zerns", {}))
-        pupil = pupil_mag * np.exp(1j*pupil_phase)
         
-        self.pupil = pupil[int(1.5*size[0]):int(-1.5*size[0]), int(1.5*size[1]):int(-1.5*size[1])]
+        self.pupil = pupil_mag * np.exp(1j*pupil_phase)        
+        self.pupil = self.pupil[int(1.5*size[0]):int(-1.5*size[0]), int(1.5*size[1]):int(-1.5*size[1])]
         
-        pupil = np.fft.fftshift(pupil)
-        shifted_pupils = pupil[None,...]*np.exp(-2j*np.pi*(self.kx*shifts[:,0,None,None] + self.ky*shifts[:,1,None,None]))
-        # shifted_pupils = np.stack([pupil,]*length)
+        shifted_pupil_phase = np.tile(pupil_phase, (shifts.shape[0], 1, 1))
+        shifted_pupil_phase = shifted_pupil_phase - 2 * np.pi * (self.KX[None,...] * shifts[:,0,None,None])
+        shifted_pupil_phase = shifted_pupil_phase - 2 * np.pi * (self.KY[None,...] * shifts[:,1,None,None])
+        if self.random_z:
+            shifted_pupil_phase = shifted_pupil_phase + np.sqrt(1-np.minimum(R, 1)**2) * shifts[:,2,None,None]        
         
-        psfs = np.fft.fftshift(np.fft.ifft2(shifted_pupils))
+        # pupil = np.fft.fftshift(pupil)
+        # shifted_pupils = pupil[None,...]*np.exp(-2j*np.pi*(self.kx*shifts[:,0,None,None] + self.ky*shifts[:,1,None,None]))
+        # # shifted_pupils = np.stack([pupil,]*length)
+        shifted_pupils = pupil_mag[None,...]*np.exp(1j*shifted_pupil_phase)
+        
+        psfs = np.fft.ifftshift(np.fft.ifft2(np.fft.fftshift(shifted_pupils)))
         psfs = psfs[:, int(1.5*size[0]):int(-1.5*size[0]), int(1.5*size[1]):int(-1.5*size[1])]
         psfs = np.abs(psfs)**2
         
@@ -123,6 +135,14 @@ class FourierOptics2DPSFDataset(SimulatedPSFDataset):
             
         return pupil_phase
 
+
+class FourierOptics2DPSFDataset(FourierOpticsPSFDataset):
+    def __init__(self, *args, **kwargs):
+        FourierOpticsPSFDataset.__init__(self, random_z=False, *args, **kwargs)
+        
+class FourierOptics3DPSFDataset(FourierOpticsPSFDataset):
+    def __init__(self, *args, **kwargs):
+        FourierOpticsPSFDataset.__init__(self, random_z=True, *args, **kwargs)
     
 def calculate_zernike(j, radial_distance, azimuthal_angle):
     def decode_ansi_index(j):

41d9e710d515ded0ef36b677543366c110a670ae
kkhchung <39562183+kkhchung@users.noreply.github.com>
2021-11-27 21:48:52 -0500

    Added dataset class for sampling a single image.

--- smlm_dl/dataset.py
+++ smlm_dl/dataset.py
@@ -3,6 +3,8 @@ from torch.utils.data import Dataset
 from matplotlib import pyplot as plt
 import math
 import enum
+import scipy
+from scipy import ndimage
 
 @enum.unique
 class Augmentation(enum.Enum):
@@ -71,7 +73,7 @@ class SimulatedPSFDataset(Dataset):
             psfs -= psfs.min(axis=(1,2), keepdims=True)
             psfs /= psfs.max(axis=(1,2), keepdims=True)
             
-        self.psfs = psfs.astype(np.float32)[:, None, ...]
+        self.images = psfs.astype(np.float32)[:, None, ...]
         
     def generate_psfs(self, size, length, shifts, psf_params, *args, **kwargs):
         # return np.random.uniform(0, 1, (length,) + size)
@@ -86,10 +88,10 @@ class SimulatedPSFDataset(Dataset):
         return ret
     
     def __len__(self):
-        return self.psfs.shape[0]
+        return self.images.shape[0]
 
     def __getitem__(self, key):
-        image = self.psfs[key]
+        image = self.images[key]
         label = self.shifts[key]
         
         if Augmentation.PIXEL_SHIFT in self.augmentations:
@@ -217,18 +219,116 @@ def calculate_zernike(j, radial_distance, azimuthal_angle):
     return z
         
 
-def inspect_psfs(psf_dataset, indices=None):
+def inspect_images(dataset, indices=None):
     if indices is None:
-        indices = np.random.choice(psf_dataset.psfs.shape[0], 5, replace=False)
+        indices = np.random.choice(dataset.images.shape[0], 5, replace=False)
     fig, axes = plt.subplots(1, len(indices), figsize=(4*len(indices), 3))
     for i, val in enumerate(indices):
-        im = axes[i].imshow(psf_dataset[val][0][0])
+        im = axes[i].imshow(dataset[val][0][0])
         plt.colorbar(im, ax=axes[i])
         axes[i].set_title("id: {}".format(val))
-    if hasattr(psf_dataset, 'pupil'):
+    if hasattr(dataset, 'pupil'):
         fig, axes = plt.subplots(1, 2, figsize=(4*2, 3))
-        im = axes[0].imshow(np.abs(psf_dataset.pupil))
+        im = axes[0].imshow(np.abs(dataset.pupil))
         plt.colorbar(im, ax=axes[0])
-        im = axes[1].imshow(np.angle(psf_dataset.pupil))
+        im = axes[1].imshow(np.angle(dataset.pupil))
         plt.colorbar(im, ax=axes[1])
 
+
+class SingleImageDataset(Dataset):
+    """
+    Repeatedly sample a single image.
+    """
+    def __init__(self, data, out_size=(64, 64), length=16, img_params={'A':[0.5,2.0], 'bg':[0,10], 'shifts':[5, 5], 'conv':np.ones((3,3))},
+                 noise_params={'poisson':True, 'gaussian':10}, normalize=True, augmentations={Augmentation.PIXEL_SHIFT:[8,8]}, *args, **kwargs):
+        Dataset.__init__(self)
+        
+        for key in augmentations:
+            if not isinstance(key, Augmentation):
+                raise Exception("Augmentation '{}' not recognized. Use Augmentation enum.".format(key))
+        self.augmentations = augmentations
+        self.padding = augmentations.get(Augmentation.PIXEL_SHIFT, [0,0]) # x, y
+        self.gen_size = (out_size[0]+2*self.padding[0], out_size[1]+2*self.padding[1])
+        self.out_size = out_size
+                   
+        A = img_params.get('A', [1,1])        
+        bg = img_params.get('bg', [0,0])
+        shifts = img_params.get('shifts', [out_size[0]/3-self.padding[0]/2, out_size[1]/3-self.padding[1]/2])
+        conv = img_params.get('conv', None)
+        
+        self.shifts = list([np.random.uniform(-shifts[0], shifts[0], length),
+                            np.random.uniform(-shifts[1], shifts[1], length),                                
+                            ])
+        self.shifts = np.stack(self.shifts, axis=-1)
+            
+        images = self.generate_images(data, self.gen_size, length, conv, self.shifts, *args, **kwargs)
+        
+        images = images * np.random.uniform(A[0], A[1], length)[:, None, None]
+        images = images + np.random.uniform(bg[0], bg[1], length)[:, None, None]
+        
+        if len(noise_params) > 0:
+            images = self.add_noise(images, noise_params)
+
+        if normalize:
+            images -= images.min(axis=(1,2), keepdims=True)
+            images /= images.max(axis=(1,2), keepdims=True)
+            
+        self.images = images.astype(np.float32)[:, None, ...]
+        
+    def generate_images(self, data, size, length, conv, shifts, *args, **kwargs):
+        # add padding larger than shifts
+        shift_max = [np.ceil(np.max([np.abs(shifts[:,i].min()), shifts[:,i].max()])).astype(int) for i in range(len(shifts.shape))]
+        crop_size = [size[i] + 2*shift_max[i] for i in range(len(data.shape))]
+        data = data[:crop_size[0],:crop_size[1]]
+        
+        if not conv is None:
+            data = ndimage.convolve(data, conv)
+        
+        # zero padding for fft
+        padding = [(int(np.ceil(1.5 * data.shape[0])),)*2, (int(np.ceil(1.5 * data.shape[1])),)*2]        
+        data = np.pad(data, padding)
+        
+        kx = np.fft.fftshift(np.fft.fftfreq(data.shape[0]))
+        ky = np.fft.fftshift(np.fft.fftfreq(data.shape[1]))
+        self.KX, self.KY = np.meshgrid(kx, ky, indexing='ij')
+        
+        fft_image = np.fft.fftshift(np.fft.fft(data))        
+        fft_image_mag = np.abs(fft_image)
+        fft_image_phase = np.angle(fft_image)
+        
+        fft_image_phase = fft_image_phase - 2 * np.pi * (self.KX[None,...] * shifts[:,0,None,None])
+        fft_image_phase = fft_image_phase - 2 * np.pi * (self.KY[None,...] * shifts[:,1,None,None])
+        
+        shifted_fft = fft_image_mag * np.exp(1j * fft_image_phase)
+        shifted_img = np.fft.ifft(np.fft.ifftshift(shifted_fft))
+        
+        crop = np.concatenate([shift_max[i] + padding[i] for i in range(len(data.shape))])        
+        shifted_img = shifted_img[:, crop[0]:-crop[1], crop[2]:-crop[3]]
+        
+        return np.abs(shifted_img)
+        
+    def add_noise(self, images, noise_params):
+        ret = images
+        if noise_params.pop('poisson', False):
+            ret = np.random.poisson(images)
+        if 'gaussian' in noise_params:
+            ret = np.random.normal(images, noise_params['gaussian'])
+        return ret
+    
+    def __len__(self):
+        return self.images.shape[0]
+
+    def __getitem__(self, key):
+        image = self.images[key]
+        label = self.shifts[key]
+        
+        if Augmentation.PIXEL_SHIFT in self.augmentations:
+            shift = [np.random.randint(0, 2*i+1) for i in self.padding]
+            image = image[:,shift[0]:shift[0]+self.out_size[0],shift[1]:shift[1]+self.out_size[1]]
+            label = label + shift
+            
+        if Augmentation.NOISE_GAUSSIAN in self.augmentations:
+            noise_sig = self.augmentations[Augmentation.NOISE_GAUSSIAN] * (image.max() - image.min())
+            image = np.random.normal(image, noise_sig).astype(np.float32)
+            
+        return image, label

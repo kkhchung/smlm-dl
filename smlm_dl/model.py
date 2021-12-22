f520a678f5d96453fd568fbea5db43294f6218f2
kkhchung <39562183+kkhchung@users.noreply.github.com>
2021-12-22 16:09:04 -0500

    Added U-Net encoder model.

--- smlm_dl/model.py
+++ smlm_dl/model.py
@@ -161,6 +161,83 @@ class ConvImageEncoderModel(ImageEncoderModel):
             nn.ReLU(),
         )
     
+class UnetEncoderModel(ImageEncoderModel):
+    def __init__(self, img_size=(32,32), depth=3, in_channels=1, first_layer_out_channels=16, last_out_channels=2, ):
+        if 2**depth > img_size[0] or 2**depth > img_size[1]:
+            raise Exception("Model too deep for this image size (depth = {}, image size needs to be at least ({}, {}))".format(depth, 2**depth, 2**depth))
+        
+        ImageEncoderModel.__init__(self, img_size=img_size, in_channels=in_channels,
+                                  last_out_channels=last_out_channels,
+                                  **{"depth":depth, "first_layer_out_channels":first_layer_out_channels, })
+        
+    def build_model(self, depth, first_layer_out_channels, ):        
+        self.encoders = nn.ModuleDict()
+        for i in range(depth):
+            in_channels = self.in_channels if i == 0 else 2**(i-1) * first_layer_out_channels
+            out_channels = 2**i * first_layer_out_channels
+
+            self.encoders["conv_layer{}".format(i)] = self._conv_block(in_channels, out_channels)
+            self.encoders["pool_layer{}".format(i)] = self._pool_block(out_channels, out_channels)
+            
+        self.neck = self._conv_block(int(2**(depth-1) * first_layer_out_channels),
+                                     (2**(depth) * first_layer_out_channels))        
+        
+        self.decoders = nn.ModuleDict()
+        for i in range(depth):
+            in_channels = 2**(depth - i) * first_layer_out_channels
+            out_channels = 2**(depth - i - 1) * first_layer_out_channels
+            self.decoders["up_conv_layer{}".format(i)] = self._up_conv_block(in_channels, out_channels)
+            self.decoders["conv_layer{}".format(i)] = self._conv_block(in_channels, out_channels)
+        
+        self.final_decoder = self._final_decoder(first_layer_out_channels, self.last_out_channels)
+        
+    def forward(self, x):
+        skips = list()
+        
+        for i, (key, module) in enumerate(self.encoders.items()):
+            x = module(x)
+            if i % 2 == 0: # is conv_layer
+                skips.insert(0, x)
+        
+        x = self.neck(x)
+        
+        for i, (key, module) in enumerate(self.decoders.items()):
+            if i % 2 == 1: # is conv_layer
+                x = torch.cat([skips[i//2], x], dim=1)
+            x = module(x)
+            
+        x = self.final_decoder(x)
+            
+        return x
+    
+    def _conv_block(self, in_channels, out_channels):
+        return nn.Sequential(
+            # nn.GroupNorm(in_channels, in_channels),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(),
+            # nn.GroupNorm(out_channels, out_channels),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Dropout2d(0.5),
+        )
+    
+    def _pool_block(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.MaxPool2d(2),
+        )
+    
+    def _up_conv_block(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2),
+        )
+    
+    def _final_decoder(self, in_channels, out_channels):
+        return nn.Sequential(
+            # nn.GroupNorm(in_channels, in_channels),
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0),
+            nn.ReLU(),
+        )
+    
     
 class FeedbackModel(BaseModel):
     def __init__(self, img_size=(32,32), feedback_size=(32,32)):

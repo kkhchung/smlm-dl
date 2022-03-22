16ff484b33e1e8b65ac91442f6dacfa78e732726
kkhchung <39562183+kkhchung@users.noreply.github.com>
2022-03-22 19:12:00 -0400

    Fixed camera MLE loss model.

--- smlm_dl/loss.py
+++ smlm_dl/loss.py
@@ -1,7 +1,11 @@
 from functools import partial
 
+import numpy as np
+
 import torch
 from torch import nn
+
+import model
 
 class CameraLoss(nn.Module):
     def __init__(self, mle, gain=1, offset=100):
@@ -60,7 +64,7 @@ class GaussMLECameraLoss(CameraLoss):
     
     def forward(self, input, target, reduction='mean'):
         target = self._to_photons(target)
-        loss = nn.functional.gaussian_nll_loss(input, target, torch.ones_like(input)*self.var, reduction=reduction)
+        loss = nn.functional.gaussian_nll_loss(input, target, torch.ones_like(input)*self.var, full=True, reduction=reduction)
         return loss
     
 
@@ -77,49 +81,97 @@ class GaussMLEPoissonWeightedCameraLoss(CameraLoss):
 
 
 class GaussPoissonMLECameraLoss(CameraLoss):
-    def __init__(self, gain=1, offset=100, var=0):
+    def __init__(self, gain=1, offset=100, var=0, debug=False):
         super().__init__(mle=True, gain=gain, offset=offset)
         self.var = var
+        self.debug = debug
+        if self.debug:
+            self.cached_images = dict()
+            self.optim_counts = 0
     
     def forward(self, input, target, reduction='mean'):
         target = self._to_photons(target)
         
         pos_diff = target - input
         input_poisson = torch.clamp(input, 0)
-        poisson_var_proportion = torch.sqrt(input_poisson / (input_poisson + self.var))
-        self.noise = nn.ParameterDict({
-            'poisson' : nn.Parameter(torch.clamp(input_poisson + pos_diff * poisson_var_proportion, 0))
+        poisson_std_proportion = torch.sqrt(input_poisson / (input_poisson + self.var))
+        self.prop_poisson_noise = nn.ModuleDict({
+            "base": model.base.ParameterModule(-torch.log(1/poisson_std_proportion-1)),
+            "clip": nn.Sigmoid(),
         })
-        self.optimizer = torch.optim.LBFGS(self.noise.values(), lr=0.5,
-                                      max_iter=10, tolerance_grad=1e-3,
-                                     tolerance_change=1e-3,
+        self.optimizer = torch.optim.LBFGS(self.prop_poisson_noise["base"].parameters(), lr=0.5,
+                                           max_iter=50,
+                                           tolerance_grad=1e-1,
+                                           tolerance_change=1e-3,
                                            history_size=10)
-            
+        
+        if self.debug:
+            self.optim_counts = 0
         self.optimizer.step(partial(self._nested_closure, input=input.detach(), target=target.detach()))
+        if self.debug:
+            print(self.optim_counts)
         self.optimizer.zero_grad()
-        loss = self._calculate_nll(input, target)
+        loss = self._calculate_nll(input, target, round_poisson=True)
         
         return loss
     
-    def _calculate_nll(self, input, target):
+    def _calculate_nll(self, input, target, round_poisson=False):
         eps = 1e-8
         poisson_input = torch.clamp(input, 0)
-        target_with_poisson = self.noise['poisson'] - torch.clamp(self.noise['poisson'].detach(), None, 0)
-        # the built-in Stirling approx is breaking autograd
-        poisson_nll = nn.functional.poisson_nll_loss(poisson_input, target_with_poisson, log_input=False, full=False, eps=eps, reduction='mean')
+
+        prop_poisson_noise = None
+        for module in self.prop_poisson_noise.values():
+            prop_poisson_noise = module(prop_poisson_noise)
+        error = target - input
+        target_clipped = target - torch.clamp(target, None, 0)
+        poisson_noise = target_clipped - input
+        poisson_noise[input <= 0] = 0
+        poisson_noise = poisson_noise * prop_poisson_noise
+        if round_poisson is True:
+            poisson_noise = torch.round(poisson_noise)
         
-        stirling_term = target_with_poisson * torch.log(target_with_poisson + eps) - target_with_poisson + 0.5 * torch.log(2 * torch.pi * target_with_poisson + eps)
-        stirling_term[target_with_poisson<=1] = 0
-        stirling_term = stirling_term.mean()
-        poisson_nll = poisson_nll + stirling_term
+        poisson_nll = self._calculate_poisson_nll(poisson_input, poisson_input + poisson_noise)
+        gaussian_nll = self._calculate_gaussian_nll(error - poisson_noise, self.var)
+        nll = poisson_nll + gaussian_nll
         
-        gaussian_noise = target - target_with_poisson + poisson_input
-        gauss_nll = nn.functional.gaussian_nll_loss(input, gaussian_noise, torch.ones_like(gaussian_noise)*self.var, full=True, reduction='mean')
+        if self.debug:
+            self.cached_images["input"] = input.detach()
+            self.cached_images["target"] = target.detach()
+            self.cached_images["poisson_noise"] = poisson_noise.detach()
+            self.cached_images["gaussian_noise"] = (error - poisson_noise).detach()
+            self.cached_images["nll"] = nll.detach()
+            print('noises', poisson_noise[0,0,0,0], error[0,0,0,0] - poisson_noise[0,0,0,0], error[0,0,0,0])
+            print('p', poisson_noise[0,0,0,0] / error[0,0,0,0])
         
-        return poisson_nll + gauss_nll
+        nll = nll.mean()
+        
+        return nll
         
     def _nested_closure(self, input, target):
         self.optimizer.zero_grad()
         loss = self._calculate_nll(input, target)
         loss.backward()
+        if self.debug:
+            self.optim_counts = self.optim_counts + 1
         return loss
+    
+    def _calculate_poisson_nll(self, input, target, eps=1e-8):
+        poisson_nll_a = input - target * torch.log(input + eps) 
+        # poisson_nll_b = target * np.log(target) - target + 0.5 * np.log(2*torch.pi*target)
+        poisson_nll_b = torch.lgamma(target + 1)
+        if self.debug:
+            print('poisson nll', poisson_nll_a[0,0,0,0], poisson_nll_b[0,0,0,0])
+            # print('poisson nll', poisson_nll_a.squeeze(), poisson_nll_b.squeeze(), poisson_nll_a.squeeze() + poisson_nll_b.squeeze())
+            self.cached_images["poisson_nll_a"] = poisson_nll_a.detach()
+            self.cached_images["poisson_nll_b"] = poisson_nll_b.detach()
+        return poisson_nll_a + poisson_nll_b
+    
+    def _calculate_gaussian_nll(self, error, var):
+        gaussian_nll_a = 0.5 * (np.log(var) + (error**2 / var))
+        gaussian_nll_b = 0.5 * np.log(2*np.pi)
+        if self.debug:
+            print('gaussian nll', gaussian_nll_a[0,0,0,0], gaussian_nll_b)
+            # print('gaussian nll', gaussian_nll_a.squeeze(), gaussian_nll_b.squeeze(), gaussian_nll_a.squeeze() + gaussian_nll_b.squeeze())
+            self.cached_images["gaussian_nll_a"] = gaussian_nll_a.detach()
+            self.cached_images["gaussian_nll_b"] = gaussian_nll_b
+        return gaussian_nll_a + gaussian_nll_b
